c5e2166cb88503af5d27c1a508061b9efa784bb8
爱摸鱼的坏船长 <46187947+mrbruce516@users.noreply.github.com>
2020-11-09 17:12:07 +0800

    成功转变为函数式编程，可一次性创建多人考勤记录

--- dateGenerator.py
+++ dateGenerator.py
@@ -6,8 +6,11 @@ import xlwt
 
 # 设定开始日期并格式化
 print("输入1自行输入日期，输入其他字符自动选择上周一")
-# 定义函数
+# 定义全局数组
 dateList = []
+timeList = []
+dateGenerator = []
+# 定义生成日期时间函数
 def useDate():
     choise = input()
     if choise == str(1):
@@ -37,59 +40,61 @@ def useDate():
             dateStart += datetime.timedelta(days=+1)
             date_list.append(dateStart.strftime('%Y/%m/%d'))
 
-useDate()
-print(dateList)
+    # 创建时间
+    a = 0
+    time_list = timeList
+    while a<10:
+        moring = 8
+        eveing = random.randint(17,18)
+        mminute = random.randint(45,59)
+        eminute = random.randint(30,59)
+        second = random.randint(10,59)
+        mtime = str(0) + str(moring) + ":" + str(mminute) + ":" + str(second)
+        etime = str(eveing) + ":" + str(eminute) + ":" + str(second)
+        a = a+1
+        time_list.append(mtime)
+        time_list.append(etime)
+    
+# 拼接日期与时间函数
+def splice():
+    date_generator = dateGenerator
+    # 工作日
+    dateFreq = 0
+    # 早上和晚上的时间
+    timeFreq = 0
+    # 一周工作日5天
+    while dateFreq < 5:
+        date_generator.append(dateList[dateFreq] + " " + timeList[timeFreq])
+        timeFreq += 1
+        date_generator.append(dateList[dateFreq] + " " + timeList[timeFreq])
+        dateFreq += 1
+        timeFreq += 1
 
-## 创建时间
-#a = 0
-#time_list = []
-#while a<10:
-#    moring = 8
-#    eveing = random.randint(17,18)
-#    mminute = random.randint(45,59)
-#    eminute = random.randint(30,59)
-#    second = random.randint(10,59)
-#    mtime = str(0) + str(moring) + ":" + str(mminute) + ":" + str(second)
-#    etime = str(eveing) + ":" + str(eminute) + ":" + str(second)
-#    a = a+1
-#    time_list.append(mtime)
-#    time_list.append(etime)
-#
-## 拼接日期与时间
-#dateGenerator = []
-#dateGenerator.append(date_list[0] + " " + time_list[0])
-#dateGenerator.append(date_list[0] + " " + time_list[1])
-#dateGenerator.append(date_list[1] + " " + time_list[2])
-#dateGenerator.append(date_list[1] + " " + time_list[3])
-#dateGenerator.append(date_list[2] + " " + time_list[4])
-#dateGenerator.append(date_list[2] + " " + time_list[5])
-#dateGenerator.append(date_list[3] + " " + time_list[6])
-#dateGenerator.append(date_list[3] + " " + time_list[7])
-#dateGenerator.append(date_list[4] + " " + time_list[8])
-#dateGenerator.append(date_list[4] + " " + time_list[9])
-#
-## 创建xls文件
-#wb = xlwt.Workbook()
-#
-#sh1 = wb.add_sheet('日期')
-#
-#sh1.write(0,0,'日期')
-#sh1.write(1,0,dateGenerator[0])
-#sh1.write(2,0,dateGenerator[1])
-#sh1.write(3,0,dateGenerator[2])
-#sh1.write(4,0,dateGenerator[3])
-#sh1.write(5,0,dateGenerator[4])
-#sh1.write(6,0,dateGenerator[5])
-#sh1.write(7,0,dateGenerator[6])
-#sh1.write(8,0,dateGenerator[7])
-#sh1.write(9,0,dateGenerator[8])
-#sh1.write(10,0,dateGenerator[9])
-#
-#wb.save('日期.xls')
-#
-## 校验文件是否存在
-#check = os.path.isfile("日期.xls")
-#if check == True:
-#    print("已成功完成任务")
-#else:
-#    print("遇到迷之问题。。")
+# 创建函数完毕并调用
+useDate()
+# 创建xls文件
+wb = xlwt.Workbook()
+
+sh1 = wb.add_sheet('日期')
+
+people = int(input("请输入需要生成的打卡人数"))
+# 生成时间库
+timeLib = 0
+while timeLib < people:
+    splice()
+    timeLib += 1
+
+genFreq = people * 10
+xlsLine = 0
+while xlsLine < genFreq:
+    sh1.write(xlsLine,0,dateGenerator[xlsLine])
+    xlsLine += 1
+
+wb.save('日期.xls')
+
+# 校验文件是否存在
+check = os.path.isfile("日期.xls")
+if check == True:
+    print("已成功完成任务")
+else:
+    print("遇到迷之问题。。")
